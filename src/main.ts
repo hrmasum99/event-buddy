c04e024c418f4ede304bcc7515b56f36d04dce17
Habibur Rahman Masum <hrmasum99.study@gmail.com>
2025-08-04 15:35:27 +0600

    updated

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
+  app.enableCors({
+    // origin: [
+    //   'http://localhost:3000', // for local dev
+    //   'https://your-frontend.vercel.app' // replace this with your actual Vercel domain
+    // ],
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Event Buddy API')
     .setDescription('Backend APIs for Event Booking System')
