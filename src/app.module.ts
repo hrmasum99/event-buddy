d221c66397a5192de4baf07cafee2071dd6a29ec
Habibur Rahman Masum <hrmasum99.study@gmail.com>
2025-08-04 11:45:15 +0600

    Initial commit in main branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,26 @@
-import { ConfigModule } from "@nestjs/config";
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { AuthModule } from "./auth/auth.module";
-import { UsersModule } from "./users/users.module";
-import { EventsModule } from "./events/events.module";
-import { BookingsModule } from "./bookings/bookings.module";
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { EventsModule } from './events/events.module';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: process.env.dbhost,
-    port: +process.env.dbport,
-    username: process.env.dbuser,
-    password: process.env.dbpass,
-    database: process.env.dbname,
-    autoLoadEntities: true,
-    synchronize: true, }),
+      type: 'postgres',
+      host: process.env.dbhost,
+      port: parseInt(process.env.dbport),
+      username: process.env.dbuser,
+      password: process.env.dbpass,
+      database: process.env.dbname,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     AuthModule,
     UsersModule,
     EventsModule,
