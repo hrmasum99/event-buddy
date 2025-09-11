0f3f14573059551b1885fbd7164d526b6eb6e856
Habibur Rahman Masum <hrmasum99.study@gmail.com>
2025-09-11 15:54:13 +0600

    Port added

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 7000);
+  // await app.listen(process.env.PORT || 7000);
+  await app.listen(process.env.PORT || 7000, '0.0.0.0');
 }
 bootstrap();
